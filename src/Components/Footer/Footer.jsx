aa8520572350d267b78b44ce446164a3c48d43fd
Saikaranam-70 <saikaranam46@gmail.com>
2025-02-27 15:04:07 +0530

    Your commit message

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -33,16 +33,16 @@ const Footer = () => {
           <h3>Follow Us</h3>
           <div className="social-icons">
             <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-              <FaFacebook />
+              Facebook<FaFacebook />
             </a>
             <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-              <FaTwitter />
+              Twitter<FaTwitter />
             </a>
             <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-              <FaInstagram />
+              Instagram<FaInstagram />
             </a>
             <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-              <FaLinkedin />
+              LinkedIn<FaLinkedin />
             </a>
           </div>
         </div>
