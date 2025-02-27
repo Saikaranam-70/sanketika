b6d88f2e145723e912e1768ea6ea8fdb130a370d
Saikaranam-70 <saikaranam46@gmail.com>
2025-02-27 15:57:34 +0530

    Your commit message

--- src/Components/Courses/Courses.jsx
+++ src/Components/Courses/Courses.jsx
@@ -5,32 +5,31 @@ import civil from '../../../src/assets/civil.jpg';
 import 'aos/dist/aos.css';
 import Aos from 'aos';
 import { useEffect } from "react";
-
+import eee from '../../assets/eee.jpeg'
+import cme from '../../assets/cme.jpeg'
+import ece from '../../assets/ece.jpeg'
+import mech from '../../assets/mech.jpeg'
+import Ai from '../../assets/Ai.jpeg'
+import Ae from '../../assets/Ae.jpeg'
 const courses = [
-  { name: "Electrical Engineering", image: "https://images.unsplash.com/photo-1519389950473-47ba0277781c", link: "/eee" },
-  { name: "Computer Engineering", image: "https://images.unsplash.com/photo-1546410531-bb4caa6b424d", link: "/cme" },
-  { name: "Electronics & Communication", image: "https://images.unsplash.com/photo-1518770660439-4636190af475", link: "/ece" },
-  { name: "Mechanical Engineering", image: "https://images.unsplash.com/photo-1519638399535-1b036603ac77", link: "/mech" },
-  { name: "Artificial Intelligence & ML", image: "https://images.unsplash.com/photo-1555949963-ff9fe0c870eb", link: "/aiml" },
+  { name: "Electrical Engineering", image: eee, link: "/eee" },
+  { name: "Computer Engineering", image: cme, link: "/cme" },
+  { name: "Electronics & Communication", image: ece, link: "/ece" },
+  { name: "Mechanical Engineering", image: mech, link: "/mech" },
+  { name: "Artificial Intelligence & ML", image: Ai, link: "/aiml" },
   { name: "Civil Engineering", image: civil, link: "/civil" },
-  { name: "Automobile Engineering", image: "https://images.unsplash.com/photo-1533473359331-0135ef1b58bf", link: "/automobile" },
+  { name: "Automobile Engineering", image: Ae, link: "/automobile" },
 ];
 
 const Courses = () => {
-  useEffect(() => {
-    Aos.init({
-      duration: 500,
-      easing: 'ease-in-out',
-      once: false,
-    });
-  }, []);
+  
 
   return (
-    <div className="courses-container" data-aos="fade-right">
+    <div className="courses-container" >
       <h2 className="courses-title">Our Courses</h2>
-      <div className="courses-grid" data-aos="fade-left">
+      <div className="courses-grid" >
         {courses.map((course, index) => (
-          <Link key={index} to={course.link} className="course-card" data-aos="fade-up">
+          <Link key={index} to={course.link} className="course-card" >
             <img src={course.image} alt={course.name} className="course-image" />
             <h3 className="course-name">{course.name}</h3>
           </Link>
